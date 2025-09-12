caa7958dec10999f22fa737c8418249af02a5e4a
Ben <53241776+benNthen@users.noreply.github.com>
2025-09-12 20:48:01 +1200

    Catch-All fallback routes & dealing with multiple segments

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,24 +1,63 @@
 import NewsList from '@/components/news-list'
 import Link from 'next/link'
-import { getAvailableNewsYears, getNewsForYear } from '@/lib/news'
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from '@/lib/news'
 
 export default function FilteredNewsPage({ params }) {
   const filter = params.filter
-  console.log(filter) // you can see in terminal the current [year] we're in
 
-  const links = getAvailableNewsYears()
+  //notations to access first stored value
+  //longer alt. code is tertinary operator is filter ? filter [0] : undefined
+  const selectedYear = filter?.[0]
+  const selectedMonth = filter?.[1]
+
+  let news
+  let links = getAvailableNewsYears()
+
+  // Conditional - shows news if a year is selected among the choices
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear)
+    links = getAvailableNewsMonths(selectedYear)
+  }
+
+  //  Conditional - displays the specific news for a specific year and month
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth)
+    links = []
+  }
+
+  // Conditional - otherwise display this message if nothing is picked
+
+  let newsContent = <p>Select a year to view news.</p>
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />
+  }
 
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`
+
+              return (
+                <li key={link}>
+                  <Link href={`/archive/${link}`}>{link}</Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   )
 }
