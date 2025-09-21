dcbb6de3010a4cb3b57c3284c8e13634d4636dac
Ben <53241776+benNthen@users.noreply.github.com>
2025-09-21 16:49:24 +1200

    installed better-sqlite3 for separate back-end and connected app to it instead of DUMMY_NEWS

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,18 +1,10 @@
 /* News Page - shows a list of news item links that lead to a News Detail page*/
 
 import NewsList from '@/components/news-list'
+import { getAllNews } from '@/lib/news'
 
 export default async function NewsPage() {
-  const response = await fetch('http://localhost:3000/news') // API address
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch news.')
-  }
-
-  const text = await response.text()
-  console.log('Raw response:', text)
-
-  const news = JSON.parse(text)
+  const news = getAllNews()
 
   return (
     <>
